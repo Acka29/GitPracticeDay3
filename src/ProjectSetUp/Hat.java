e2251c924e21e85a70e8b49df2bc7c43aa2afa90
aleksandrajonoska <jonoska.aleksandra@yahoo.com>
2023-01-05 12:19:52 -0700

    Hat added

---
+++ src/ProjectSetUp/Hat.java
@@ -0,0 +1,11 @@
+package ProjectSetUp;
+
+public class Hat {
+
+    public static void main(String[] args) {
+
+        System.out.println("The store for hats is located on Lemaay Street");
+
+
+    }
+}
