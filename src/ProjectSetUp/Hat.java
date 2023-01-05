40236a3cb5b0b065d97758c7274b44b1293a787d
aleksandrajonoska <jonoska.aleksandra@yahoo.com>
2023-01-05 12:20:33 -0700

    Hat class some changes added

--- src/ProjectSetUp/Hat.java
+++ src/ProjectSetUp/Hat.java
@@ -6,6 +6,7 @@ public class Hat {
 
         System.out.println("The store for hats is located on Lemaay Street");
 
+        System.out.println("I was there last week and the prices are great as well");
 
     }
 }
