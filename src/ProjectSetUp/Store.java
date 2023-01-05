c58fc73b07571cfa79c305b28e4a5bab29545044
aleksandrajonoska <jonoska.aleksandra@yahoo.com>
2023-01-05 12:31:12 -0700

    Store updated

--- src/ProjectSetUp/Store.java
+++ src/ProjectSetUp/Store.java
@@ -5,6 +5,7 @@ public class Store {
     public static void main(String[] args) {
 
         System.out.println("They just opened ZARA in Cherry Creek,store is very modern ");
+        System.out.println("I love Zara, they have great things");
 
     }
 }
