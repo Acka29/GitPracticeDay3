1f0067f337ac6aa233d5421b9c89f492766a1bb9
aleksandrajonoska <jonoska.aleksandra@yahoo.com>
2023-01-05 12:29:51 -0700

    Store added

---
+++ src/ProjectSetUp/Store.java
@@ -0,0 +1,10 @@
+package ProjectSetUp;
+
+public class Store {
+
+    public static void main(String[] args) {
+
+        System.out.println("They just opened ZARA in Cherry Creek,store is very modern ");
+
+    }
+}
