ac33d91aafc5546317fcff28be64d0cfdc84abaf
aleksandrajonoska <jonoska.aleksandra@yahoo.com>
2023-01-05 12:12:57 -0700

    Water and Food added

---
+++ src/ProjectSetUp/Food.java
@@ -0,0 +1,10 @@
+package ProjectSetUp;
+
+public class Food {
+
+    public static void main(String[] args) {
+
+        System.out.println("Best recipes for pizza");
+
+    }
+}
---
+++ src/ProjectSetUp/Water.java
@@ -0,0 +1,12 @@
+package ProjectSetUp;
+
+public class Water {
+
+    public static void main(String[] args) {
+
+        System.out.println("The prices for bottled water are going up, $0.50 cents");
+
+
+
+    }
+}
